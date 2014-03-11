8eafa2c70830637633923007db5e858c1ee38d2d
Masaya Tk <844196+sourcetree@gmail.com>
2014-03-11 23:11:23 +0900

    mac_setup.shのタイポを修正

--- mac_setup.sh
+++ mac_setup.sh
@@ -5,6 +5,10 @@ ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.vimshrc $HOME/.vimshrc
 ln -s $HOMW/dotfiles/.zshrc $HOME/.zshrc
 
+#NeoBundle
+mkdir -p $HOME/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+
 #Homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew bundle
@@ -16,8 +20,7 @@ curl -L -o Inconsolata.otf "http://levien.com/type/myfonts/Inconsolata.otf"
 curl -L -o migu-1m.zip "http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fmix-mplus-ipa%2F59022%2Fmigu-1m-20130617.zip"
 unzip -j migu-1m.zip
 sh ricty_generator.sh Inconsolata.otf migu-1m-regular.ttf migu-1m-bold.ttf
-cp -f Ricty*.ttf $HOME/.fonts/
-% fc-cache -vf
+cp Ricty*.ttf $HOME/Library/Fonts/
 cd $HOME
 rm -rf $HOME/Ricty
 
