ad5cc49954ecf1122705ea20b6287f7492d2b04a
Masaya Tk <844196+sourcetree@gmail.com>
2014-03-14 17:13:11 +0900

    localip.sh追加, weather.shを修正

--- weather.sh
+++ weather.sh
@@ -1,16 +1,30 @@
 #/bin/sh
-get=$(curl http://www.accuweather.com/ja/jp/asahikawa-shi/223986/weather-forecast/223986 2> /dev/null | grep -e 'span\ class="cond"' | sed -e 's//\\n/g' | head -n1 | sed -e 's/<[^>]*>//g' | sed -e 's/\ //')
+cityname='Monbetsu'
+unit="°C"
 
-IFS=' '
+get=$(curl http://api.openweathermap.org/data/2.5/weather?q=$cityname,jp\&units=metric\&lang=en 2>/dev/null | jq '.name, .weather[].icon, .main.temp, .main.temp_max, .main.temp_min, .wind.speed')
+
+IFS='\
+    '
 set -- $get
 
-case $1 in
-    *晴れ)   sign='☀ ';;
-    *曇り)   sign='☁ ';;
-    *雨)   sign='☂ ';;
-    *雪)   sign='☃ ';;
+location=$(echo $1 | sed -e 's/"//g')
+cond=$(echo $2 | sed -e 's/"//g')
+wind="⚐"$6"m"
+
+case $cond in
+    '01d'|'01n'|'02d'|'02n')
+        sign='☀ ';;
+    '03d'|'03n'|'04d'|'04n')
+        sign='☁ ';;
+    '09d'|'09n'|'10d'|'10n'|'11d'|'11n')
+        sige='☂ ';;
+    '13d'|'13n')
+        sign='☃ ';;
+    '50d'|'50n')
+        sign='〰';;
+    *)
+        sign='?';;
 esac
 
-temp=$(echo $2 | sed -e 's/&deg;//g')
-
-printf "$sign $temp"°C"" > $HOME/.weather
+echo "$location\n$sign\n$3$unit\n$4$unit\n$5$unit\n$wind">$HOME/.weather
