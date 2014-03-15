69065abab809de5a426f06df6063d0cf4afec4d7
Masaya Tk <844196+sourcetree@gmail.com>
2014-03-15 15:34:34 +0900

    修正

--- weather.sh
+++ weather.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 cityname='Monbetsu'
+appid='153937e5a40ad0ee0fb19d31b18bc7ad'
 
-unit='°C'
+unit=( '°C' 'm' )
 LF=$(printf '\\\\012')
 
-get=$(curl "http://api.openweathermap.org/data/2.5/weather?q=$cityname,jp&units=metric&lang=en" 2>/dev/null | sed -e 's/,/'"$LF"'/g' -e 's/"//g' -e 's/{//g' -e 's/}//g' -e 's/\[//g' -e 's/\]//g')
+get=$(curl "http://api.openweathermap.org/data/2.5/weather?q=${cityname},jp&units=metric&lang=en&APPID=${appid}" 2>/dev/null | sed -e 's/,/'"$LF"'/g' -e 's/"//g' -e 's/{//g' -e 's/}//g' -e 's/\[//g' -e 's/\]//g')
 
-location=`echo $get | grep -e 'name:' | sed -e 's/name://'`
-temp=`echo $get | grep -e 'main:temp:' | sed -e 's/main:temp://'`
-tempmax=`echo $get | grep -e 'temp_max:' | sed -e 's/temp_max://'`
-tempmin=`echo $get | grep -e 'temp_min:' | sed -e 's/temp_min://'`
-wind=`echo $get | grep -e 'wind:speed:' | sed -e 's/wind:speed://'`
+location=$(echo $get | grep -e 'name:' | sed -e 's/name://')
+temp=$(echo $get | grep -e 'main:temp:' | sed -e 's/main:temp://')${unit[0]}
+tempmax=$(echo $get | grep -e 'temp_max:' | sed -e 's/temp_max://')${unit[0]}
+tempmin=$(echo $get | grep -e 'temp_min:' | sed -e 's/temp_min://')${unit[0]}
+wind='⚐'$(echo $get | grep -e 'wind:speed:' | sed -e 's/wind:speed://')${unit[1]}
 
-cond=`echo $get | grep -e 'icon:' | sed -e 's/icon://'`
+cond=$(echo $get | grep -e 'icon:' | sed -e 's/icon://')
 case $cond in
     '01d'|'01n'|'02d'|'02n')
         sign='☀ ';;
@@ -28,4 +29,5 @@ case $cond in
         sign='?';;
 esac
 
-echo "$location\n$sign\n$temp$unit\n$tempmax$unit\n$tempmin$unit\n⚐${wind}m" >$HOME/.weather
+echo "$location\n$sign\n$temp\n$tempmax\n$tempmin\n${wind}" >$HOME/.weather
+#echo "@$location $sign $temp "↑ "$tempmax "↓ "$tempmin ${wind}"
