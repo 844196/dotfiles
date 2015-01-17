102130dd89f277e2005c5c50609bd5d5c1044a71
Masaya Takeda <844196@gmail.com>
2015-01-18 05:30:32 +0900

    fix setup.sh and change file directory

--- setup.sh
+++ setup.sh
@@ -1,49 +1,222 @@
-#!/bin/sh
+#!/bin/bash
+#
+#   @(#) Dotfiles setup scripts
+#
+#   Usage:
+#       $ curl -sL 844196.com/dot | bash
+#
+#   Inspired:
+#       https://github.com/Kuniwak/dotfiles/blob/master/setup.sh
+#       https://github.com/b4b4r07/dotfiles/blob/master/etc/install
+#
+#   Author:
+#       844196 (@84____)
+#
+#   License:
+#       WTFPL
+#
 
 
-# 導入するドットファイルを宣言
+# initialize
 
-    dotfiles=(.vimrc .gvimrc .vimshrc .zshrc .zshenv .tmux.conf .gitconfig .gitignore_global)
+    function printLogo() {
+        clear
+        local d="$(tput smso)  $(tput rmso)"
+        cat <<-EOF
+	
+	                $d                    $d$d  $d  $d$d
+	                $d            $d      $d          $d
+	                $d    $d    $d$d$d  $d$d$d  $d    $d    $d$d    $d$d
+	              $d$d  $d  $d    $d      $d    $d    $d  $d$d$d  $d$d
+	            $d  $d  $d  $d    $d      $d    $d    $d  $d        $d$d
+	              $d$d    $d      $d$d    $d    $d    $d    $d$d  $d$d
+	
+		EOF
+    }
+    printLogo
 
+    # message wrapper
+    function messageHeader() {
+        local str="${*}"
+        # shellcheck disable=2034
+        local padding="$(
+            for i in $( seq 0 $(( 78 - ${#str} )) )
+            do
+                printf " "
+            done
+        )"
+        echo -en "\n$(tput smso) ${*}${padding}$(tput rmso)\n"
+    }
 
-# 環境の判定
+    function messageSuccess() { echo "  $(tput setaf 2)✔ $(tput sgr0) ${*}"; }
+    function messageArrow()   { echo "  $(tput setaf 6)➜ $(tput sgr0) ${*}"; }
+    function messageError()   { echo "  $(tput setaf 1)✖ $(tput sgr0) ${*}" 1>&2; }
 
-    if [ $(uname) = 'Darwin' ]; then ismac='0'; fi
+    # common function
+    function executableCommand() {
+        type "${1}" >/dev/null 2>&1
+        return "${?}"
+    }
 
+    function existsFile() {
+        [[ -e "${1}" ]]
+        return "${?}"
+    }
 
-# ドットファイルをクローン
+    function makeSymlink() {
+        local from="${1}"
+        local to="${2}"
 
-    git clone https://github.com/844196/dotfiles ~/dotfiles
-
-
-# ドットファイルのシンボリックリンクを作成
-
-    for file in ${dotfiles[@]}; do
-        ln -s ~/dotfiles/${file} ~/${file}
-    done
-
-
-# NeoBundleをインストール
-
-    if which git >/dev/null 2>&1; then
-        if ! [ -e ~/.vim/bundle/neobundle.vim ]; then
-            mkdir -p ~/.vim/bundle
-            git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+        if existsFile "${to}"; then
+            messageError "File already exists -- '${to}'"
+            return 2
+        else
+            ln -s "${from}" "${to}" >/dev/null 2>&1
+            if [[ "${?}" -eq 0 ]]; then
+                messageSuccess "Successful make symboliclink -- '${to}'"
+                return 0
+            else
+                messageError "Failed make symboliclink -- '${to}'"
+                return 1
+            fi
         fi
+    }
+
+    function cloneRepository() {
+        local from="${1}"
+        local to="${2}"
+        local name="${3}"
+
+        messageArrow "Downloading ${3}..."
+        git clone -q "${from}" "${to}" >/dev/null
+        if [[ "${?}" -eq 0 ]]; then
+            messageSuccess "Successful download -- '${to}'"
+            return 0
+        else
+            messageError "Failed download --'${name}'"
+            return 1
+        fi
+    }
+
+
+# check executable git
+
+    if executableCommand "git"; then
+        :
+    else
+        messageHeader "Check executable git command"
+        messageError "This setup script required git"
+        exit 1
     fi
 
 
-# Macなら以下を実行
+# download dotfiles or sync
 
-    if [ -n "${ismac}" ]; then
-        # Homebrewをインストール
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-        # ClipMenu拡張のシンボリックリンクを張る
-        mkdir -p ~/Library/Application\ Support/ClipMenu/script/action
-        cp ~/dotfiles/javascript/*.js ~/Library/Application\ Support/ClipMenu/script/action
-
-        # Karabinerリマップのシンボリックリンクを張る
-        mkdir -p ~/Library/Application\ Support/Karabiner
-        ln -s ~/dotfiles/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+    if existsFile "${HOME}/dotfiles"; then
+        messageHeader "Sync remote branch"
+        messageArrow "Running... 'git pull --rebase'"
+        (
+            set -e
+            cd "${HOME}/dotfiles"
+            c="$(git rev-parse --abbrev-ref HEAD)"
+            git checkout -q master >/dev/null
+            git pull --rebase >/dev/null
+            git checkout -q "${c}"
+        )
+        if [[ "${?}" -eq 0 ]]; then
+            messageSuccess "Successful dotfiles sync"
+        else
+            messageError "Failed dotfiles sync"
+            exit 1
+        fi
+    else
+        messageHeader "Download dofiles"
+        cloneRepository "https://github.com/844196/dotfiles" "${HOME}/dotfiles" "dotfiles" \
+            || exit 1
     fi
+
+
+# make symboliclink dotfiles
+
+    # zsh
+    function setupZshConfigurationFiles() {
+        makeSymlink "${HOME}/dotfiles/.zshenv" "${HOME}/.zshenv"
+
+        mkdir -p "${HOME}/.zsh"
+        makeSymlink "${HOME}/dotfiles/.zshrc" "${HOME}/.zsh/.zshrc"
+    }
+
+    # need to keep in $HOME other files
+    function setupOtherConfigurationFiles() {
+        dotfiles=('.vimrc' '.gitconfig' '.tmux.conf')
+        for file in "${dotfiles[@]}"
+        do
+            makeSymlink "${HOME}/dotfiles/${file}" "${HOME}/${file}"
+        done
+    }
+
+    if existsFile "${HOME}/.zsh/.zshrc_local" || existsFile "${HOME}/.vim/.vimrc_local"; then
+        :
+    else
+        messageHeader "Make symboliclink dotfiles"
+
+        setupZshConfigurationFiles
+        setupOtherConfigurationFiles
+    fi
+
+
+# install zsh-syntax-highlighting
+
+    if existsFile "${HOME}/.zsh/zsh-syntax-highlighting"; then
+        :
+    else
+        messageHeader "Install zsh-syntax-highlighting"
+
+        mkdir -p "${HOME}/.zsh"
+        cloneRepository \
+            "https://github.com/zsh-users/zsh-syntax-highlighting" \
+            "${HOME}/.zsh/zsh-syntax-highlighting" \
+            "zsh-syntax-highlighting"
+    fi
+
+
+# install neobundle
+
+    if existsFile "${HOME}/.vim/bundle/neobundle.vim"; then
+        :
+    else
+        messageHeader "Install NeoBundle"
+
+        mkdir -p ~/.vim/bundle
+        cloneRepository \
+            "https://github.com/Shougo/neobundle.vim" \
+            "${HOME}/.vim/bundle/neobundle.vim" \
+            "NeoBundle"
+    fi
+
+
+# mac only
+
+    function isMacHomebrewUpdate() {
+        messageHeader "Update Homebrew"
+
+        messageArrow "Running... 'brew update && brew upgrade'"
+        brew update >/dev/null && brew upgrade >/dev/null
+        if [[ "${?}" -eq 0 ]]; then
+            messageSuccess "Successful Homebrew update"
+        else
+            messageError "Failed homebrew update"
+        fi
+    }
+
+    function isMac() {
+        if executableCommand "brew"; then
+            isMacHomebrewUpdate
+        else
+            messageHeader "Homebrew"
+            messageError "This environment has not been introduced Homebrew"
+            messageArrow "This setup script does not provide the installation process"
+        fi
+    }
+
+    [[ "$(uname)" = "Darwin" ]] && isMac
