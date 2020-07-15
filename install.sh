5bf286ea32adbc8b57c5cb60eb78ce1c51915922
Masaya Takeda <844196@gmail.com>
2020-07-15 18:21:48 +0900

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+git clone https://github.com/844196/dotfiles ~/.dotfiles
 
+ln -sfn ~/.dotfiles/zsh ~/.zsh
+ln -sfn ~/.dotfiles/zsh/.zshenv ~/.zshenv
+
+mkdir -p ~/.config
+ln -sfn ~/.dotfiles/git ~/.config/git
