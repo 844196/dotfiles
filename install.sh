797f7f7b6980b589eca87acdb92097ee5febf2a8
Masaya Takeda <844196@gmail.com>
2020-07-15 18:58:07 +0900

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-git clone https://github.com/844196/dotfiles ~/.dotfiles
+if ! which git >/dev/null 2>&1; then
+  echo "${cmd} is not installed" >&2
+  exit 1
+fi
 
-ln -sfn ~/.dotfiles/zsh ~/.zsh
-ln -sfn ~/.dotfiles/zsh/.zshenv ~/.zshenv
+for cmd in zsh fzf; do
+  which $cmd >/dev/null 2>&1 || echo "${cmd} is not installed" >&2
+done
+
+git clone https://github.com/844196/dotfiles ~/.dotfiles
 
 mkdir -p ~/.config
 ln -sfn ~/.dotfiles/git ~/.config/git
+
+ln -sfn ~/.dotfiles/zsh ~/.zsh
+ln -sfn ~/.dotfiles/zsh/.zshenv ~/.zshenv
