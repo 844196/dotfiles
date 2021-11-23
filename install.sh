d83c0d582db6b952e47c64ede1bab0ae80d02d6e
Masaya Takeda <844196@gmail.com>
2021-11-24 03:49:31 +0900

    💚 fix install.sh for remote containers

--- install.sh
+++ install.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
-if ! which git >/dev/null 2>&1; then
-  echo 'Git is not installed' >&2
-  exit 1
+set -e
+
+if [ "$(command -v chezmoi)" ]; then
+  chezmoi=chezmoi
+else
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b ~/.local/bin
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b ~/.local/bin
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+  chezmoi=~/.local/bin/chezmoi
 fi
 
-[ -e ~/.dotfiles ] || git clone --depth 1 https://github.com/844196/dotfiles ~/.dotfiles
-
-cp -f ~/.dotfiles/.exports ~/.exports
-
-cp -f ~/.dotfiles/.bashrc ~/.bashrc
-cp -f ~/.dotfiles/.inputrc ~/.inputrc
-
-cp -f ~/.dotfiles/.zshenv ~/.zshenv
-cp -f ~/.dotfiles/.zsh/.zshrc ~/.zsh/.zshrc
-
-mkdir -p ~/.config/git
-cp -f ~/.dotfiles/.gitconfig ~/.gitconfig
-cp -f ~/.dotfiles/.config/git/ignore ~/.config/git/ignore
-
-cp -f ~/.dotfiles/.vimrc ~/.vimrc
-
-mkdir -p ~/.local/bin
-cp -f ~/.dotfiles/.local/bin/git-credential-manager ~/.local/bin/git-credential-manager
-
-[ -e ~/.fzf ] || git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --bin
-ln -sf ~/.fzf/bin/fzf ~/.local/bin/fzf
-
-mkdir -p ~/.zsh/plugins
-[ -e ~/.zsh/plugins/zsh-syntax-highlighting ] || git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/plugins/zsh-syntax-highlighting
-[ -e ~/.zsh/plugins/zsh-autosuggestions ] || git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
-
-[ -e ~/.local/bin/starship ] || sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --bin-dir ~/.local/bin --yes
-mkdir -p ~/.config
-cp -f ~/.dotfiles/.config/starship.toml ~/.config/starship.toml
+dotfiles="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+exec "$chezmoi" init --apply "--source=$dotfiles"
