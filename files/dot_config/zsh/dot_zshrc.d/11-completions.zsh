a3646315a0b0b25fdaf718ecab894376a5d63341
Masaya Takeda <844196@gmail.com>
2022-02-09 01:38:20 +0900

    ⚡️ enable cache

--- files/dot_config/zsh/dot_zshrc.d/11-completions.zsh
+++ files/dot_config/zsh/dot_zshrc.d/11-completions.zsh
@@ -6,6 +6,8 @@ setopt magic_equal_subst
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z} r:|[-_.]=**' '+m:{A-Z}={a-z} r:|[-_.]=**'
 zstyle ':completion:*:default' menu select=2
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path $XDG_CACHE_HOME/zsh/compcache
 
 if [ -e $XDG_DATA_HOME/zsh/plugins/zsh-completions ]; then
   fpath=($XDG_DATA_HOME/zsh/plugins/zsh-completions/src(N-/) $fpath)
