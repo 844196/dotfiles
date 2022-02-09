7661d94a879bb877d9a49a3af8f258b721b0cc9b
Masaya Takeda <844196@gmail.com>
2022-02-09 15:05:19 +0900

    🐛 remove line break

--- files/dot_config/zsh/exact_dot_zshrc.d/32-less-options.zsh
+++ files/dot_config/zsh/exact_dot_zshrc.d/32-less-options.zsh
@@ -1,10 +1,3 @@
-export LESS='
-  --no-init
-  --RAW-CONTROL-CHARS
-  --ignore-case
-  --tabs=4
-  --chop-long-lines
-  --HILITE-UNREAD
-'
+export LESS='--no-init --RAW-CONTROL-CHARS --ignore-case --tabs=4 --chop-long-lines --HILITE-UNREAD'
 export LESSHISTFILE=-
 export LESSCHARSET=utf-8
