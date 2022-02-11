c93db4c318145caa07e10be05d9e7e185dd3f369
Masaya Takeda <844196@gmail.com>
2022-02-12 05:37:36 +0900

    ♿️ fzf preview position consider terminal size

--- files/dot_config/zsh/exact_dot_zshrc.d/31-fzf-options.zsh
+++ files/dot_config/zsh/exact_dot_zshrc.d/31-fzf-options.zsh
@@ -11,8 +11,6 @@ export FZF_DEFAULT_OPTS="
   --marker='❯'
   --query \"'\"
   --border=horizontal
-  --preview-window 'bottom,80%,border-top'
-  --bind '?:change-preview-window(hidden|right,75%,border-left|bottom,80%,border-top)'
 "
 
 # see: https://gist.github.com/sheepla/d1ff1ef11cc21dcc7434a25a012a970f
