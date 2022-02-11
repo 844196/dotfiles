17ffd23f8c5f5261037f7f45572c47cc76c24e91
Masaya Takeda <844196@gmail.com>
2022-02-11 21:16:32 +0900

    📝 include duplicate command line in zsh history

--- files/dot_config/zsh/exact_dot_zshrc.d/12-histories.zsh
+++ files/dot_config/zsh/exact_dot_zshrc.d/12-histories.zsh
@@ -3,19 +3,4 @@ HISTSIZE=1000
 SAVEHIST=100000
 
 setopt share_history
-setopt hist_no_store
 setopt hist_reduce_blanks
-setopt hist_ignore_all_dups
-
-zshaddhistory() {
-  local line=${1%%$'\n'}
-  local cmd=${line%% *}
-
-  [[ ${#line} -ge 5
-    && $cmd != (cd|gp|pwd)
-    && $cmd != (ls|exa)
-    && $cmd != (less|bat)
-    && $line != *--(help|version)
-    && $line != (git status|git graph)
-  ]]
-}
