f9226b9b8a3c6d233761e554d2005348ef47d354
Masaya Takeda <844196@gmail.com>
2022-02-13 07:47:47 +0900

    💄 transparent fzf background

--- files/dot_config/zsh/exact_dot_zshrc.d/31-fzf-options.zsh
+++ files/dot_config/zsh/exact_dot_zshrc.d/31-fzf-options.zsh
@@ -15,7 +15,7 @@ export FZF_DEFAULT_OPTS="
 
 # see: https://gist.github.com/sheepla/d1ff1ef11cc21dcc7434a25a012a970f
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
-  --color=bg:#161821
+  --color=bg:-1
   --color=bg+:#1e2132
   --color=fg:#c6c8d1
   --color=fg+:#c6c8d1
@@ -24,7 +24,7 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
   --color=spinner:#84a0c6
   --color=header:#6b7089
   --color=border:#34394E
-  --color=gutter:#161821
+  --color=gutter:-1
   --color=info:#b4be82
   --color=pointer:#84a0c6
   --color=marker:#84a0c6
