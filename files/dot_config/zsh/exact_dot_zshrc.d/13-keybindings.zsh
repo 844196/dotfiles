53a1f99684e9f02fdc4bbf33126307468a3e18c4
Masaya Takeda <844196@gmail.com>
2022-02-14 02:18:20 +0900

    🔥 remove unused keybinds

--- files/dot_config/zsh/exact_dot_zshrc.d/13-keybindings.zsh
+++ files/dot_config/zsh/exact_dot_zshrc.d/13-keybindings.zsh
@@ -3,8 +3,6 @@ autoload -Uz dot-widget-init && dot-widget-init
 bindkey '^[[Z' reverse-menu-complete
 bindkey '^M' dot-accept-line-and-expand-alias
 bindkey ' ' dot-self-insert-and-expand-alias
-bindkey '^[[1;2A' dot-pushd-up-and-reset-prompt
-bindkey '^[[1;2B' dot-popd-and-reset-prompt
 bindkey '^R' dot-put-history
 bindkey '^X^B' dot-insert-git-branch
 bindkey '^X^F' dot-insert-git-file
