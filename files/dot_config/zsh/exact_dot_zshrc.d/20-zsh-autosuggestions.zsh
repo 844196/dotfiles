bcd5552d6212f9513b8598a528a156324c542344
Masaya Takeda <844196@gmail.com>
2022-02-15 10:24:55 +0900

    🐛 fix accept-line

--- files/dot_config/zsh/exact_dot_zshrc.d/20-zsh-autosuggestions.zsh
+++ files/dot_config/zsh/exact_dot_zshrc.d/20-zsh-autosuggestions.zsh
@@ -1,6 +1,6 @@
 if [ -e $XDG_DATA_HOME/zsh/plugins/zsh-autosuggestions ]; then
   ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=black,bold"
-  ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(dot-accept-line-and-expand-alias)
+  ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(expand-alias-and-accept-line)
 
   source $XDG_DATA_HOME/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 fi
