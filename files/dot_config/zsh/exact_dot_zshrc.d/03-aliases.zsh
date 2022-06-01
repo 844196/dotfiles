900c0c356f77d269a9e7e5519a87d2865631303e
Masaya Takeda <844196@gmail.com>
2022-06-02 00:15:00 +0900

    :fire: remove unnecessary alias

--- files/dot_config/zsh/exact_dot_zshrc.d/03-aliases.zsh
+++ files/dot_config/zsh/exact_dot_zshrc.d/03-aliases.zsh
@@ -12,9 +12,7 @@ alias gg='git graph -n 15'
 
 alias c='chezmoi'
 alias m='multipass'
-alias o='ouch'
 alias curl='curl -fsSL'
-alias grep='grep -E'
 
 if [ -n "${commands[exa]}" ]; then
   alias ls='exa -gF --icons --git --color=always -la'
