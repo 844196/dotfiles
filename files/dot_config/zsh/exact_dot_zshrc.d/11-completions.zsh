842429d4a93cfc882e21a7575fa5ef351a11aa9f
Masaya Takeda <844196@gmail.com>
2022-07-08 23:57:48 +0900

    :bug: fix completion color

--- files/dot_config/zsh/exact_dot_zshrc.d/11-completions.zsh
+++ files/dot_config/zsh/exact_dot_zshrc.d/11-completions.zsh
@@ -5,6 +5,5 @@ setopt magic_equal_subst
 
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z} r:|[-_.]=**' '+m:{A-Z}={a-z} r:|[-_.]=**'
 zstyle ':completion:*:default' menu select=2
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path $XDG_CACHE_HOME/zsh/compcache
