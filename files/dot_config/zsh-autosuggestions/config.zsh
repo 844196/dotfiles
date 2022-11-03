5ac9913874215ad14eb11d7939b5a889d2925544
Masaya Takeda <844196@gmail.com>
2022-11-04 08:32:01 +0900

    :ambulance: Fix config files not loading

--- files/dot_config/zsh-autosuggestions/config.zsh
+++ files/dot_config/zsh-autosuggestions/config.zsh
@@ -1,2 +1,2 @@
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=black,bold"
-export ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(dot-expand-alias-and-accept-line)
+export ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(dot-expand-alias-and-accept-line $ZSH_AUTOSUGGEST_CLEAR_WIDGETS)
