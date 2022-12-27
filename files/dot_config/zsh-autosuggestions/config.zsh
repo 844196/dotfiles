e3ba1dbd3949ea6eb134d2c68313e16d7ebdf284
Masaya Takeda <844196@gmail.com>
2022-12-27 23:47:19 +0900

    :sparkles: Introduce options auto suggestion in CLI

--- files/dot_config/zsh-autosuggestions/config.zsh
+++ files/dot_config/zsh-autosuggestions/config.zsh
@@ -1,2 +1,3 @@
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=black,bold"
 export ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(dot-expand-alias-and-accept-line $ZSH_AUTOSUGGEST_CLEAR_WIDGETS)
+export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
