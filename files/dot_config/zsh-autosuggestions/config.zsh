36c63abd448890678250270f36f75f72c7af0e5d
Masaya Takeda <844196@gmail.com>
2023-02-15 00:39:43 +0900

    :children_crossing: Search history from a line beginning

--- files/dot_config/zsh-autosuggestions/config.zsh
+++ files/dot_config/zsh-autosuggestions/config.zsh
@@ -1,3 +1,11 @@
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=black,bold"
-export ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(dot-expand-alias-and-accept-line $ZSH_AUTOSUGGEST_CLEAR_WIDGETS)
+export ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(
+  dot-expand-alias-and-accept-line
+
+  # SEE: https://github.com/zsh-users/zsh-autosuggestions/issues/619
+  history-beginning-search-backward-end
+  history-beginning-search-forward-end
+
+  $ZSH_AUTOSUGGEST_CLEAR_WIDGETS
+)
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
