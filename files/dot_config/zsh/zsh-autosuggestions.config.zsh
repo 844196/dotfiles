58bbce85e64da560250606eec217c2886bd5f516
Masaya Takeda <844196@gmail.com>
2023-02-21 04:29:35 +0900

    :truck: Move files

--- files/dot_config/zsh/zsh-autosuggestions.config.zsh
+++ files/dot_config/zsh/zsh-autosuggestions.config.zsh
@@ -1,12 +1,10 @@
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=black,bold"
-export ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(
-  dot-expand-alias-and-accept-line
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=black,bold"
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(
+  accept-line-with-expand-alias
 
   # SEE: https://github.com/zsh-users/zsh-autosuggestions/issues/619
   history-beginning-search-backward-end
   history-beginning-search-forward-end
-
-  $ZSH_AUTOSUGGEST_CLEAR_WIDGETS
 )
-export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
