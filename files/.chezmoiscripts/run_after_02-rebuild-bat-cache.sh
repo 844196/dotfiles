6577c2e1671ec9a6cf2ae0904a46c77e21fbe809
844196 <844196@users.noreply.github.com>
2023-12-09 19:20:39 +0900

    :bug: Fix aqua path

--- files/.chezmoiscripts/run_after_02-rebuild-bat-cache.sh
+++ files/.chezmoiscripts/run_after_02-rebuild-bat-cache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bat="$(~/.local/bin/aqua -c ~/.config/aquaproj-aqua/aqua.yaml which bat)"
+bat="$(~/.local/share/aquaproj-aqua/bin/aqua -c ~/.config/aquaproj-aqua/aqua.yaml which bat)"
 
 exec "$bat" cache --build >/dev/null
