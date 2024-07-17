aa6e11ca0b65d9747575f0196c2a4c640b603bd5
844196 <844196@users.noreply.github.com>
2024-07-18 02:48:51 +0900

    :zap: Compile zsh files on `chezmoi apply`

---
+++ files/.chezmoiscripts/run_after_06-compile-zsh-files.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+local files=(
+  ~/.config/zsh/.{zshenv,zshrc}
+  ~/.local/share/zsh/plugins/zsh-autosuggestions/**/*.zsh
+  ~/.local/share/zsh/plugins/fast-syntax-highlighting/**/*.zsh
+  ~/.local/share/zsh/plugins/fast-syntax-highlighting/{fast-highlight,fast-string-highlight,fast-theme}
+)
+
+local f
+for f in $files; do
+  echo "Compiling $f"
+  zcompile $f
+done
