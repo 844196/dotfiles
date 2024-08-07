8fddd4bb8609514c55df6a584ac4831c6001b164
844196 <844196@users.noreply.github.com>
2024-08-08 02:52:30 +0900

    :construction:

--- files/dot_local/exact_libexec/wk.ts
+++ files/dot_local/exact_libexec/wk.ts
@@ -2,12 +2,19 @@ import { colors } from "https://deno.land/x/cliffy@v1.0.0-rc.4/ansi/colors.ts";
 import { tty } from "https://deno.land/x/cliffy@v1.0.0-rc.4/ansi/tty.ts";
 import { KeyCode, parse } from "https://deno.land/x/cliffy@v1.0.0-rc.4/keycode/mod.ts";
 import { border as defaultBorder, Table } from "https://deno.land/x/cliffy@v1.0.0-rc.4/table/mod.ts";
-import { parse as parseYaml } from "jsr:@std/yaml";
+import { Color as ColorUtil } from "https://deno.land/x/color@v0.3.0/mod.ts";
+import { emojify } from "https://deno.land/x/github_emoji@v0.1.1/mod.ts";
+import { deepMerge } from "jsr:@std/collections@1.0.5";
+import { join as joinPath } from "jsr:@std/path@1.0.2";
+import { parse as parseYaml } from "jsr:@std/yaml@1.0.2";
+import { rcFile } from "npm:rc-config-loader@4.1.3";
+import { xdgConfig } from "npm:xdg-basedir@5.1.0";
+import { z } from "npm:zod@3.23.8";
 
 type Binding =
   & {
     key: string;
-    name: string;
+    desc: string;
     icon?: string;
   }
   & ({
@@ -19,74 +26,173 @@ type Binding =
     bindings: Binding[];
   });
 
-const ASCII = /^[ -~]$/;
+const XDG_CONFIG_HOME = xdgConfig ?? Deno.makeTempDirSync();
 
-const OUTPUT_DELIMITER = Deno.env.get("WK_OUTPUT_DELIMITER") ?? "\t";
+const bindings = (() => {
+  return parseYaml(Deno.readTextFileSync(joinPath(XDG_CONFIG_HOME, "wk", "bindings.yaml"))) as Binding[];
+})();
 
-const UI_SYMBOLS = {
-  PROMPT: Deno.env.get("WK_SYMBOL_PROMPT") ?? " ",
-  BREADCRUMB: Deno.env.get("WK_SYMBOL_BREADCRUMB") ?? " » ",
-  SEPARATOR: Deno.env.get("WK_SYMBOL_SEPARATOR") ?? "➜",
-  GROUP: Deno.env.get("WK_SYMBOL_GROUP") ?? "+",
-} as const;
+const ANSI256ColorSchema = z.union([
+  z.number().int().min(-1).max(255),
+  z.string().regex(/^#[0-9a-fA-F]{6}$/),
+]);
+const ColorSchema = z.union([
+  ANSI256ColorSchema,
+  z.object({
+    color: ANSI256ColorSchema,
+    attrs: z.array(z.enum(["bold", "dim", "italic", "underline", "inverse", "hidden", "strikethrough"])).min(1),
+  }),
+]);
+type Color = z.infer<typeof ColorSchema>;
 
-const KEY_SYMBOLS: Record<string, string> = {
-  "space": "␣",
-  "escape": "⎋",
-  "return": "⏎",
-  "tab": "⇥",
-  "up": "↑",
-  "down": "↓",
-  "right": "→",
-  "left": "←",
-  "home": "⇱",
-  "end": "⇲",
-  "pageUp": "⇞",
-  "pageDown": "⇟",
-  "delete": "⌦",
-};
+const ContextSchema = z.object({
+  continueOnUndefinedKey: z.boolean(),
+  outputDelimiter: z.string(),
+  symbols: z.object({
+    prompt: z.string(),
+    breadcrumb: z.string(),
+    separator: z.string(),
+    group: z.string(),
+    keys: z.record(z.string()),
+  }),
+  colors: z.object({
+    prompt: ColorSchema,
+    breadcrumb: ColorSchema,
+    separator: ColorSchema,
+    group: ColorSchema,
+    inputKeys: ColorSchema,
+    lastInputKey: ColorSchema,
+    bindingKey: ColorSchema,
+    bindingIcon: ColorSchema,
+    bindingDescription: ColorSchema,
+  }),
+});
+type Context = z.infer<typeof ContextSchema>;
+
+const config = (() => {
+  const found = rcFile("wk", { configFileName: joinPath(XDG_CONFIG_HOME, "wk", "config") }) ?? { config: {} };
+  const parsed = ContextSchema.partial().safeParse(found.config);
+
+  return parsed.success ? parsed.data : {};
+})();
+
+const context = deepMerge<Context>({
+  continueOnUndefinedKey: false,
+  outputDelimiter: "\t",
+  symbols: {
+    prompt: " ",
+    breadcrumb: " » ",
+    separator: "➜",
+    group: "+",
+    keys: {
+      space: "␣",
+      return: "⏎",
+      tab: "⇥",
+      up: "↑",
+      down: "↓",
+      right: "→",
+      left: "←",
+      home: "⇱",
+      end: "⇲",
+      pageup: "⇞",
+      pagedown: "⇟",
+      insert: "⎀",
+      delete: "⌦",
+      F1: "󱊫",
+      F2: "󱊬",
+      F3: "󱊭",
+      F4: "󱊮",
+      F5: "󱊯",
+      F6: "󱊰",
+      F7: "󱊱",
+      F8: "󱊲",
+      F9: "󱊳",
+      F10: "󱊴",
+      F11: "󱊵",
+      F12: "󱊶",
+      ...config.symbols?.keys,
+    },
+  },
+  colors: {
+    prompt: 8,
+    breadcrumb: {
+      color: 8,
+      attrs: ["dim"],
+    },
+    separator: {
+      color: 8,
+      attrs: ["dim"],
+    },
+    group: 8,
+    inputKeys: 8,
+    lastInputKey: -1,
+    bindingKey: -1,
+    bindingIcon: 8,
+    bindingDescription: 8,
+  },
+}, config);
+
+const PRINTABLE_ASCII = /^[ -~]$/;
 
 function getSymbol(key: string) {
-  return Deno.env.get(`WK_SYMBOL_${key.toUpperCase()}`) ?? KEY_SYMBOLS[key] ?? key;
+  return context.symbols.keys[key] ?? key;
 }
 
-// --------------------------------------------------------------------------------
+function color(text: string, givenColor: Color) {
+  const ansi256 = (typeof givenColor === "number" || typeof givenColor === "string") ? givenColor : givenColor.color;
+  const attrs = (typeof givenColor === "number" || typeof givenColor === "string") ? [] : givenColor.attrs;
 
-const [ttyReader, ttyWriter] = await Promise.all([
-  Deno.open("/dev/tty", { read: true, write: false }),
-  Deno.open("/dev/tty", { read: false, write: true }),
-]);
+  const colorized = ansi256 === -1
+    ? text
+    : typeof ansi256 === "string"
+    ? colors.rgb24(text, ColorUtil.string(ansi256).rgbNumber())
+    : colors.rgb8(text, ansi256);
 
-const myTty = tty({
-  reader: ttyReader,
-  writer: ttyWriter,
-});
-
-async function* keypress(): AsyncGenerator<KeyCode, void> {
-  while (true) {
-    const data = new Uint8Array(8);
-
-    ttyReader.setRaw(true);
-    const nread = await ttyReader.read(data);
-    ttyReader.setRaw(false);
-
-    if (nread === null) {
-      return;
-    }
-
-    const keys: Array<KeyCode> = parse(data.subarray(0, nread));
-
-    for (const key of keys) {
-      yield key;
+  let attributed = colorized;
+  for (const attr of attrs) {
+    switch (attr) {
+      case "bold":
+        attributed = colors.bold(attributed);
+        break;
+      case "dim":
+        attributed = colors.dim(attributed);
+        break;
+      case "italic":
+        attributed = colors.italic(attributed);
+        break;
+      case "underline":
+        attributed = colors.underline(attributed);
+        break;
+      case "inverse":
+        attributed = colors.inverse(attributed);
+        break;
+      case "hidden":
+        attributed = colors.hidden(attributed);
+        break;
+      case "strikethrough":
+        attributed = colors.strikethrough(attributed);
+        break;
     }
   }
+
+  return attributed;
 }
 
-const bindings = parseYaml(
-  Deno.readTextFileSync(`${Deno.env.get("XDG_CONFIG_HOME") ?? "~/.config"}/wk/bindings.yaml`),
-) as Binding[];
+function renderPrompt(givenKeys: string[]) {
+  const prompt = color(context.symbols.prompt, context.colors.prompt);
 
-const initCursorPos = myTty.getCursorPosition();
+  const keys: string[] = [];
+  for (let i = 0; i < givenKeys.length; i++) {
+    const key = givenKeys[i];
+    keys.push(
+      color(key, i === givenKeys.length - 1 ? context.colors.lastInputKey : context.colors.inputKeys),
+    );
+  }
+
+  return `${prompt}${keys.join(color(context.symbols.breadcrumb, context.colors.breadcrumb))}${
+    givenKeys.length > 0 ? " " : ""
+  }`;
+}
 
 const plainBorder = Object.entries(defaultBorder)
   .reduce<Record<string, string>>(
@@ -96,19 +202,52 @@ const plainBorder = Object.entries(defaultBorder)
     },
     {},
   );
-function toTable(bindings: Binding[]) {
-  return Table
-    .from(
-      bindings.map((item) => [
-        colors.white(getSymbol(item.key)),
-        colors.brightBlack(
-          // TODO emojify
-          `${item.icon ?? ""}${item.type === "bindings" ? UI_SYMBOLS.GROUP : ""}${item.name}`,
-        ),
-      ]),
-    )
-    .border(true).chars({ ...plainBorder, middle: colors.brightBlack.dim(UI_SYMBOLS.SEPARATOR) });
+
+function renderTableRow(binding: Binding) {
+  const icon = typeof binding.icon === "string" ? color(emojify(binding.icon), context.colors.bindingIcon) : "";
+  const group = binding.type === "bindings" ? color(context.symbols.group, context.colors.group) : "";
+  const desc = color(binding.desc, context.colors.bindingDescription);
+
+  return [
+    color(getSymbol(binding.key), context.colors.bindingKey),
+    `${icon}${group}${desc}`,
+  ];
 }
+
+function renderTable(bindings: Binding[]) {
+  return Table
+    .from(bindings.map(renderTableRow))
+    .border(true).chars({ ...plainBorder, middle: color(context.symbols.separator, context.colors.separator) });
+}
+
+// --------------------------------------------------------------------------------
+
+const [ttyReader, ttyWriter] = await Promise.all([
+  Deno.open("/dev/tty", { read: true, write: false }),
+  Deno.open("/dev/tty", { read: false, write: true }),
+]);
+const myTty = tty({ reader: ttyReader, writer: ttyWriter });
+
+async function* keypress(): AsyncGenerator<KeyCode, void> {
+  while (true) {
+    const data = new Uint8Array(8);
+
+    ttyReader.setRaw(true);
+    const numberOfBytesRead = await ttyReader.read(data);
+    ttyReader.setRaw(false);
+
+    if (numberOfBytesRead === null) {
+      return;
+    }
+
+    const keys: Array<KeyCode> = parse(data.subarray(0, numberOfBytesRead));
+    for (const key of keys) {
+      yield key;
+    }
+  }
+}
+
+const initCursorPos = myTty.getCursorPosition();
 
 // zle -M などによってプロンプト下に出力が残っている可能性があるため、あらかじめ表示予定領域をクリアしておく
 const o = myTty.cursorHide.eraseLine.cursorNextLine;
@@ -117,24 +256,25 @@ for (let i = 0; i < bindings.length; i++) {
 }
 o.cursorTo(initCursorPos.x, initCursorPos.y);
 
+const inputKeys: string[] = [];
+const navigation: [Binding[], ...(Binding[])[]] = [bindings];
+
 myTty
   .cursorHide
   .eraseLine
-  .text(colors.brightBlack(UI_SYMBOLS.PROMPT)).cursorSave
+  .text(renderPrompt(inputKeys)).cursorSave
   .cursorNextLine
-  .text(toTable(bindings).toString())
+  .text(renderTable(bindings).toString())
   .cursorRestore
   .cursorShow();
 
-const moveStack: [Binding[], ...(Binding[])[]] = [bindings];
-const inputKeys = [];
 for await (const key of keypress()) {
   // TODO タイムアウト
 
   // Failed to parse key
   if (typeof key.name === "undefined") {
     const o = myTty.cursorHide.eraseLine.cursorNextLine;
-    for (let i = 0; i < moveStack.at(-1)!.length; i++) {
+    for (let i = 0; i < navigation.at(-1)!.length; i++) {
       o.eraseLine.cursorNextLine();
     }
     o.cursorTo(initCursorPos.x, initCursorPos.y).cursorShow();
@@ -147,7 +287,7 @@ for await (const key of keypress()) {
   // ^C or ^D (EOF) or ^[ (Escape) => 終了
   if ((key.ctrl && key.name === "c") || (key.ctrl && key.name === "d") || (key.name === "escape")) {
     const o = myTty.cursorHide.eraseLine.cursorNextLine;
-    for (let i = 0; i < moveStack.at(-1)!.length; i++) {
+    for (let i = 0; i < navigation.at(-1)!.length; i++) {
       o.eraseLine.cursorNextLine();
     }
     o.cursorTo(initCursorPos.x, initCursorPos.y).cursorShow();
@@ -158,9 +298,9 @@ for await (const key of keypress()) {
   // ^H (Backspace) or ^W (Delete word) => 一階層戻る
   if (key.name === "backspace" || (key.ctrl && key.name === "w")) {
     // TODO 連打してると意図せず終了して鬱陶しいかもしれないので、無視するオプションを追加
-    if (moveStack.length === 1) {
+    if (navigation.length === 1) {
       const o = myTty.cursorHide.eraseLine.cursorNextLine;
-      for (let i = 0; i < moveStack.at(-1)!.length; i++) {
+      for (let i = 0; i < navigation.at(-1)!.length; i++) {
         o.eraseLine.cursorNextLine();
       }
       o.cursorTo(initCursorPos.x, initCursorPos.y).cursorShow();
@@ -168,7 +308,7 @@ for await (const key of keypress()) {
       Deno.exit(1);
     }
 
-    const currentBindings = moveStack.pop()!;
+    const currentBindings = navigation.pop()!;
 
     const o = myTty.cursorHide.eraseLine.cursorNextLine;
     for (let i = 0; i < currentBindings.length; i++) {
@@ -180,14 +320,10 @@ for await (const key of keypress()) {
 
     const n = myTty
       .cursorHide
-      .text(
-        `${colors.brightBlack(UI_SYMBOLS.PROMPT)}${inputKeys.join(colors.brightBlack(UI_SYMBOLS.BREADCRUMB))}${
-          inputKeys.length > 0 ? " " : ""
-        }`,
-      )
+      .text(renderPrompt(inputKeys))
       .cursorSave
       .cursorNextLine;
-    for (const line of toTable(moveStack.at(-1)!).toString().split("\n")) {
+    for (const line of renderTable(navigation.at(-1)!).toString().split("\n")) {
       n.eraseLine.text(line).cursorNextLine;
     }
     n.cursorRestore.cursorShow();
@@ -197,12 +333,12 @@ for await (const key of keypress()) {
 
   // ^U (Delete line) => 一番最初の階層に戻る
   if (key.ctrl && key.name === "u") {
-    if (moveStack.length === 1) {
+    if (navigation.length === 1) {
       continue;
     }
 
-    const currentBindings = moveStack.pop()!;
-    moveStack.splice(1);
+    const currentBindings = navigation.pop()!;
+    navigation.splice(1);
 
     const o = myTty.cursorHide.eraseLine.cursorNextLine;
     for (let i = 0; i < currentBindings.length; i++) {
@@ -213,14 +349,10 @@ for await (const key of keypress()) {
     inputKeys.length = 0;
     const n = myTty
       .cursorHide
-      .text(
-        `${colors.brightBlack(UI_SYMBOLS.PROMPT)}${inputKeys.join(colors.brightBlack(UI_SYMBOLS.BREADCRUMB))}${
-          inputKeys.length > 0 ? " " : ""
-        }`,
-      )
+      .text(renderPrompt(inputKeys))
       .cursorSave
       .cursorNextLine;
-    for (const line of toTable(moveStack.at(-1)!).toString().split("\n")) {
+    for (const line of renderTable(navigation.at(-1)!).toString().split("\n")) {
       n.eraseLine.text(line).cursorNextLine;
     }
     n.cursorRestore.cursorShow();
@@ -229,7 +361,7 @@ for await (const key of keypress()) {
   }
 
   // 上記以外の ^L のような制御文字 => 無視
-  if (key.ctrl && ASCII.test(key.name)) {
+  if (key.ctrl && PRINTABLE_ASCII.test(key.name)) {
     continue;
   }
 
@@ -238,10 +370,10 @@ for await (const key of keypress()) {
 
   // 未定義キー => エラーにして終了
   // TODO 無視するオプションを追加
-  const match = moveStack.at(-1)!.find((b) => b.key === inputKey);
+  const match = navigation.at(-1)!.find((b) => b.key === inputKey);
   if (!match) {
     const o = myTty.cursorHide.eraseLine.cursorNextLine;
-    for (let i = 0; i < moveStack.at(-1)!.length; i++) {
+    for (let i = 0; i < navigation.at(-1)!.length; i++) {
       o.eraseLine.cursorNextLine();
     }
     o.cursorTo(initCursorPos.x, initCursorPos.y).cursorShow();
@@ -255,7 +387,7 @@ for await (const key of keypress()) {
   if (match.type === "command") {
     const {
       key: _,
-      name: __,
+      desc: __,
       icon: ___,
       type: ____,
       buffer,
@@ -264,7 +396,7 @@ for await (const key of keypress()) {
     } = match;
 
     const o = myTty.cursorHide.eraseLine.cursorNextLine;
-    for (let i = 0; i < moveStack.at(-1)!.length; i++) {
+    for (let i = 0; i < navigation.at(-1)!.length; i++) {
       o.eraseLine.cursorNextLine();
     }
     o.cursorTo(initCursorPos.x, initCursorPos.y).cursorShow();
@@ -286,7 +418,7 @@ for await (const key of keypress()) {
       }
     }
 
-    const delimiter = typeof definedDelimiter === "string" ? definedDelimiter : OUTPUT_DELIMITER;
+    const delimiter = typeof definedDelimiter === "string" ? definedDelimiter : context.outputDelimiter;
     console.log(outputs.join(delimiter));
 
     Deno.exit(0);
@@ -295,7 +427,7 @@ for await (const key of keypress()) {
   // マッチ: バインディング => 一階層進む
   if (match.type === "bindings") {
     const o = myTty.cursorHide.eraseLine.cursorNextLine;
-    for (let i = 0; i < moveStack.at(-1)!.length; i++) {
+    for (let i = 0; i < navigation.at(-1)!.length; i++) {
       o.eraseLine.cursorNextLine();
     }
     o.cursorTo(initCursorPos.x, initCursorPos.y);
@@ -303,18 +435,14 @@ for await (const key of keypress()) {
     // TODO 画面がチラつく
     const n = myTty
       .cursorHide
-      .text(
-        `${colors.brightBlack(UI_SYMBOLS.PROMPT)}${inputKeys.join(colors.brightBlack(UI_SYMBOLS.BREADCRUMB))}${
-          inputKeys.length > 0 ? " " : ""
-        }`,
-      )
+      .text(renderPrompt(inputKeys))
       .cursorSave
       .cursorNextLine;
-    for (const line of toTable(match.bindings).toString().split("\n")) {
+    for (const line of renderTable(match.bindings).toString().split("\n")) {
       n.eraseLine.text(line).cursorNextLine;
     }
     n.cursorRestore.cursorShow();
 
-    moveStack.push(match.bindings);
+    navigation.push(match.bindings);
   }
 }
