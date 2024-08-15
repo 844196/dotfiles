88a0cfe1e032d62a8269bb2d3a1f9296a546db9c
844196 <844196@users.noreply.github.com>
2024-08-16 04:03:07 +0900

    :construction:

---
+++ files/dot_local/exact_libexec/git-add-A.ts
@@ -0,0 +1,38 @@
+import $ from "https://deno.land/x/dax@0.28.0/mod.ts";
+import { colors } from "https://deno.land/x/cliffy@v1.0.0-rc.3/ansi/colors.ts";
+
+const statuses = await $`git add -n -A`.lines();
+if (statuses.join("").trim() === "") {
+  console.log("No changes to stage.");
+  Deno.exit(0);
+}
+
+const lines = statuses.sort((a, b) => a.localeCompare(b))
+  .map((str) =>
+    str
+      .replace(/^add '/, "add ")
+      .replace(/^remove '/, "remove ")
+      .replace(/'$/, "")
+      .replace(/^add/, colors.green("add"))
+      .replace(/^remove/, colors.red("remove"))
+  );
+
+console.log(colors.black("┃"));
+console.log(`${colors.black("┃")}  ${colors.brightBlack("The following changes will be staged:")}`);
+console.log(colors.black("┃"));
+for (const line of lines) {
+  console.log(`${colors.black("┃")}    ${colors.brightBlack("•")} ${line}`);
+}
+console.log(colors.black("┃"));
+
+const response = await $.confirm(`${colors.reset(colors.black("┃"))}  ${colors.yellow("Confirm?")}`, {
+  default: true,
+  noClear: true,
+});
+
+if (response) {
+  await $`git add -A`;
+} else {
+  console.error("Aborted.");
+  Deno.exit(1);
+}
